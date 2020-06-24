a8dbb35c7e99cf5c7242e5ff36fac9a386fd6135
Bill Ashmanskas <bill.ashmanskas@gmail.com>
2020-06-24 17:11:20 -0400

    look for cocotb-config

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+if [ -e $HOME/.local/bin/cocotb-config ]; then 
+  export PATH=$HOME/.local/bin:$PATH; 
+fi
 export COCOTB_REDUCED_LOG_FMT=1
 export PYTHONDONTWRITEBYTECODE=1
 export IVERILOG_DUMPER=lxt2
